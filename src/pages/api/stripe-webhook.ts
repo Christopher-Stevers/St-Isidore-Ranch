da8d35efb9768485d5624f5542529cb3b5c60795
Christopher Stevers <christopher.stevers@protonmail.com>
2023-09-27 18:43:29 -0400

    removed method not allowed

--- src/pages/api/stripe-webhook.ts
+++ src/pages/api/stripe-webhook.ts
@@ -18,118 +18,113 @@ export default async function handler(
   req: Request,
   res: NextApiResponse,
 ) {
-  if (req.method === "GET") {
-    const sig = req.headers.get(
-      "stripe-signature",
-    ) as string;
+  const sig = req.headers.get("stripe-signature") as string;
 
-    let event: Stripe.Event;
+  let event: Stripe.Event;
 
-    try {
-      event = stripe.webhooks.constructEvent(
-        await (await req.blob()).text(),
-        sig as string,
-        webhookSecret,
-      );
+  try {
+    event = stripe.webhooks.constructEvent(
+      await (await req.blob()).text(),
+      sig as string,
+      webhookSecret,
+    );
 
-      // Handle the event
-      switch (event.type) {
-        case "payment_intent.payment_failed":
-          {
-            const paymentIntent = event.data
-              .object as Stripe.PaymentIntent;
-            // free up products
-            await prisma.product.updateMany({
-              where: {
-                Box: {
-                  Order: {
-                    paymentIntent: paymentIntent.id,
-                  },
-                },
-              },
-              data: {
-                sold: false,
-                boxId: null,
-              },
-            });
-            // delete order and boxes
-            await prisma.box.deleteMany({
-              where: {
+    // Handle the event
+    switch (event.type) {
+      case "payment_intent.payment_failed":
+        {
+          const paymentIntent = event.data
+            .object as Stripe.PaymentIntent;
+          // free up products
+          await prisma.product.updateMany({
+            where: {
+              Box: {
                 Order: {
                   paymentIntent: paymentIntent.id,
                 },
               },
-            });
-            await prisma.order.delete({
-              where: {
+            },
+            data: {
+              sold: false,
+              boxId: null,
+            },
+          });
+          // delete order and boxes
+          await prisma.box.deleteMany({
+            where: {
+              Order: {
                 paymentIntent: paymentIntent.id,
               },
-            });
-          }
-          // Used to provision services after the trial has ended.
-          // The status of the invoice will show up as paid. Store the status in your database to reference when a user accesses your service to avoid hitting rate limits.
+            },
+          });
+          await prisma.order.delete({
+            where: {
+              paymentIntent: paymentIntent.id,
+            },
+          });
+        }
+        // Used to provision services after the trial has ended.
+        // The status of the invoice will show up as paid. Store the status in your database to reference when a user accesses your service to avoid hitting rate limits.
 
-          break;
-        case "payment_intent.succeeded":
-          {
-            const paymentIntent = event.data
-              .object as Stripe.PaymentIntent;
-            const { id } = paymentIntent;
-            const order = await prisma.order.findUnique({
+        break;
+      case "payment_intent.succeeded":
+        {
+          const paymentIntent = event.data
+            .object as Stripe.PaymentIntent;
+          const { id } = paymentIntent;
+          const order = await prisma.order.findUnique({
+            where: {
+              paymentIntent: id,
+            },
+            include: {
+              boxes: {
+                include: {
+                  items: true,
+                },
+              },
+              address: true,
+            },
+          });
+          if (paymentIntent.amount === order?.totalPrice) {
+            const items = order?.boxes
+              .map((box) => box.items)
+              .flat();
+
+            await prisma.order.update({
               where: {
                 paymentIntent: id,
               },
-              include: {
-                boxes: {
-                  include: {
-                    items: true,
-                  },
-                },
-                address: true,
+              data: {
+                paid: true,
               },
             });
-            if (
-              paymentIntent.amount === order?.totalPrice
-            ) {
-              const items = order?.boxes
-                .map((box) => box.items)
-                .flat();
+            await prisma.product.updateMany({
+              where: {
+                id: {
+                  in: items?.map((item) => item.id),
+                },
+              },
+              data: {
+                sold: true,
+              },
+            });
 
-              await prisma.order.update({
-                where: {
-                  paymentIntent: id,
-                },
-                data: {
-                  paid: true,
-                },
-              });
-              await prisma.product.updateMany({
-                where: {
-                  id: {
-                    in: items?.map((item) => item.id),
+            const getProductClassOfItemById = async (
+              id: string,
+            ) => {
+              const productClass =
+                await prisma.productClass.findUnique({
+                  where: {
+                    id: id,
                   },
-                },
-                data: {
-                  sold: true,
-                },
-              });
-
-              const getProductClassOfItemById = async (
-                id: string,
-              ) => {
-                const productClass =
-                  await prisma.productClass.findUnique({
-                    where: {
-                      id: id,
-                    },
-                  });
-                return productClass?.name;
-              };
-              const htmlMessage = `<p>New order with id ${
-                order?.id
-              } and payment intent ${id} has been payed for total of ${
-                order.totalPrice
-              }
+                });
+              return productClass?.name;
+            };
+            const htmlMessage = `<p>New order with id ${
+              order?.id
+            } and payment intent ${id} has been payed for total of ${
+              order.totalPrice
+            }
                </p>
                <p>Order is for boxes:</p>
                <ul>
@@ -156,73 +151,69 @@ export default async function handler(
          <p>
                 address is:
                 <p>${order.address?.address1}</p><p>${
-                order.address?.address2
-              }</p>
+              order.address?.address2
+            }</p>
                  <p>${order.address?.postalCode}</p><p>${
-                order.address?.city
-              }</p><p>${order.address?.name}</p>
+              order.address?.city
+            }</p><p>${order.address?.name}</p>
                 .`;
-              await emailWrapper({
-                email: "christopher.stevers1@gmail.com",
-                subject: "New Order",
-                htmlMessage,
-                message: `New order with id ${order?.id} please enable html email to see full order`,
-              });
-            } else {
-              // if the order price has issues
-              await stripe.refunds.create({
-                payment_intent: id,
-              });
-              await prisma.order.update({
-                where: {
-                  paymentIntent: id,
-                },
-                data: {
-                  paid: false,
-                  paymentIntent: null,
-                },
-              });
-              await stripe.paymentIntents.cancel(id);
-              throw new Error(
-                `${paymentIntent.amount}  ${order?.totalPrice} not equal`,
-              );
-            }
+            await emailWrapper({
+              email: "christopher.stevers1@gmail.com",
+              subject: "New Order",
+              htmlMessage,
+              message: `New order with id ${order?.id} please enable html email to see full order`,
+            });
+          } else {
+            // if the order price has issues
+            await stripe.refunds.create({
+              payment_intent: id,
+            });
+            await prisma.order.update({
+              where: {
+                paymentIntent: id,
+              },
+              data: {
+                paid: false,
+                paymentIntent: null,
+              },
+            });
+            await stripe.paymentIntents.cancel(id);
+            throw new Error(
+              `${paymentIntent.amount}  ${order?.totalPrice} not equal`,
+            );
           }
-          break;
-        default:
-        // Unexpected event type
-      }
-
-      // record the event in the database
-      await prisma.stripeEvent.create({
-        data: {
-          id: event.id,
-          type: event.type,
-          object: event.object,
-          api_version: event.api_version,
-          account: event.account,
-          created: new Date(event.created * 1000), // convert to milliseconds
-          data: {
-            object: event.data.object,
-            previous_attributes:
-              event.data.previous_attributes,
-          },
-          livemode: event.livemode,
-          pending_webhooks: event.pending_webhooks,
-          request: {
-            id: event.request?.id,
-            idempotency_key: event.request?.idempotency_key,
-          },
-        },
-      });
-
-      res.json({ received: true });
-    } catch (err) {
-      res.status(400).send(err);
-      return;
+        }
+        break;
+      default:
+      // Unexpected event type
     }
-  } else {
-    res.setHeader("Allow", "POST");
-    res.status(405).end("Method Not Allowed");
+
+    // record the event in the database
+    await prisma.stripeEvent.create({
+      data: {
+        id: event.id,
+        type: event.type,
+        object: event.object,
+        api_version: event.api_version,
+        account: event.account,
+        created: new Date(event.created * 1000), // convert to milliseconds
+        data: {
+          object: event.data.object,
+          previous_attributes:
+            event.data.previous_attributes,
+        },
+        livemode: event.livemode,
+        pending_webhooks: event.pending_webhooks,
+        request: {
+          id: event.request?.id,
+          idempotency_key: event.request?.idempotency_key,
+        },
+      },
+    });
+
+    res.json({ received: true });
+  } catch (err) {
+    res.status(400).send(err);
+    return;
   }
 }
