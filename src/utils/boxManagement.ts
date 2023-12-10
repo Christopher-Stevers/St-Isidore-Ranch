21335de0420587749c2c5724112781ab2f2561c3
Christopher-Stevers <christopher.stevers@protonmail.com>
2023-12-10 07:53:08 -0500

    updated price

--- src/utils/boxManagement.ts
+++ src/utils/boxManagement.ts
@@ -200,7 +200,7 @@ class GroundBeefBoxMD implements Box {
 
   constructor() {
     this.description = LOREM_IPSUM;
-    this.totalPrice = 95;
+    this.totalPrice = 9500;
     this.variant = "small";
     this.slug = "md-ground-beef-bundle";
     this.title = `Ground Beef Bundle 
