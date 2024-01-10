c50a22ab0e3c666b3a0fbd1e016c63dd54b6470f
Christopher-Stevers <christopher.stevers@protonmail.com>
2024-01-10 09:44:17 -0500

    added hotjar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -49,6 +49,16 @@ const MyApp: AppType<{ session: Session | null }> = ({
           content="Grass fed beef | from pasture to plate"
         />
         <link rel="icon" href="/favicon.png" />
+        <script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3197536,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
       </Head>
       <SessionProvider session={session}>
         <CartProvider>
